2b7593ee57b25c4a3a9d4774773b7e066eb3300e
Jake Barnby <jakeb994@gmail.com>
2025-03-27 03:54:57 +1300

    Stable order for schema

--- pkg/converters/converters.go
+++ pkg/converters/converters.go
@@ -2,6 +2,7 @@ package converters
 
 import (
 	"github.com/wk8/go-ordered-map/v2"
+	"sort"
 )
 
 // ToAnySlice converts a slice of any type to a slice of any type.
@@ -13,11 +14,23 @@ func ToAnySlice[T any](in []T) []any {
 	return out
 }
 
-// ToOrderedProps converts a map of string to any type to an ordered map of string to any type.
+// ToOrderedProps converts a map of string to any type to an ordered map of string to any type,
+// inserting keys in sorted (alphabetical) order to ensure stable output.
 func ToOrderedProps[T any](m map[string]T) *orderedmap.OrderedMap[string, T] {
 	om := orderedmap.New[string, T]()
-	for k, v := range m {
-		om.Set(k, v)
+
+	// Gather all keys
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	// Sort them
+	sort.Strings(keys)
+
+	// Insert in sorted order
+	for _, k := range keys {
+		om.Set(k, m[k])
 	}
 	return om
 }
